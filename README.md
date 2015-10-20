e0d12d6f0b2502b183afeff80ce76fbf713dab64
Tamas Molnar <tamas.molnar@ft.com>
2015-10-20 13:53:49 +0300

    Using queue client

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # publish-availability-monitor
 Monitors publish availability and collects related metrics
+
+# Usage
+`go get github.com/Financial-Times/publish-availability-Monitors`
+
+`publish-availability-monitor -config=int-config.json`
--- app.go
+++ app.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/url"
 	"time"
+
+	"github.com/Financial-Times/go-message-queue-consumer"
 )
 
 type Interval struct {
@@ -40,12 +43,20 @@ type AppConfig struct {
 	//TODO feeder configs
 }
 
+type PublishMessageListener struct{}
+
 func main() {
 	//read config (into structs?)
 	configFileName := flag.String("config", "", "Path to configuration file")
 	flag.Parse()
 
-	ParseConfig(*configFileName)
+	appConfig, _ := ParseConfig(*configFileName)
+	//TODO handle err
+	myConsumer := consumer.NewConsumer(appConfig.QueueConf.Address, appConfig.QueueConf.Group, appConfig.QueueConf.Topic, appConfig.QueueConf.Queue)
+	err := myConsumer.Consume(PublishMessageListener{}, 8)
+	if err != nil {
+		fmt.Println(err.Error)
+	}
 	/*
 		consumer := consumer.NewConsumer(QueueConfig.address etc.)
 		scheduler := scheduler.NewScheduler()
@@ -58,13 +69,12 @@ func main() {
 	//call consume
 }
 
-/*
-func OnMessage(msg consumer.Message) error {
+func (listener PublishMessageListener) OnMessage(msg consumer.Message) error {
+	fmt.Printf("message: %v\n", msg)
 	//if message is not valid, skip
 	//generate timestamp (this is the moment we measure the publish from)
 	//scheduler.scheduleChecks(message, publishMetric)
 	//connect the scheduler with the aggregator with channels or something
 	//so when each scheduler is finished, the aggregator reads the results
-
+	return nil
 }
-*/
