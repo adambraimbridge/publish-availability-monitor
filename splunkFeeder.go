c5a67b811981e26eb612faa77727601632fa07ec
Tamas Molnar <tamas.molnar@ft.com>
2015-11-04 15:34:50 +0200

    Renamed TID field in splunk feeder

--- splunkFeeder.go
+++ splunkFeeder.go
@@ -26,6 +26,6 @@ func NewSplunkFeeder(filePath string) *SplunkFeeder {
 
 // Send logs pm into a file.
 func (sf SplunkFeeder) Send(pm PublishMetric) {
-	sf.MetricLog.Printf("UUID=%v TID=%v publishDate=%v publishOk=%v duration=%v endpoint=%v ",
+	sf.MetricLog.Printf("UUID=%v transaction_id=%v publishDate=%v publishOk=%v duration=%v endpoint=%v ",
 		pm.UUID, pm.tid, pm.publishDate.UnixNano(), pm.publishOK, pm.publishInterval.upperBound, pm.config.Alias)
 }
