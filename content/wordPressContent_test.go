ccc22b20155327c314f2e865917f576e203b119b
Yolande Leung <yolande.leung@ft.com>
2016-11-03 18:24:03 +0000

    remove dependency on APIUrl

--- content/wordPressContent_test.go
+++ content/wordPressContent_test.go
@@ -21,3 +21,24 @@ var wordpressContentMarkedDeletedTrue = WordPressMessage{
 var wordpressContentMarkedDeletedFalse = WordPressMessage{
 	Status: "ok", Post: Post{},
 }
+
+var wordpressContentWithValidBlogDomain = WordPressMessage{
+	Status: "ok", Post: Post{"post", "58bdb656-8f7a-4a8c-b2b9-f9722824b318", "http://ftalphaville.ft.com/2016/10/28/2178195/firstft-eu-canada-trade-deal-salvaged-crimes-of-the-future-and-the-price-of-fast-fashion/"},
+}
+
+var wordpressContentWithInvalidBlogDomain = WordPressMessage{
+	Status: "ok", Post: Post{"post", "58bdb656-8f7a-4a8c-b2b9-f9722824b318", "http://ftalphaville-wp.ft.com/?pid=1234"},
+}
+
+
+func TestIsValidBlogDomain_True(t *testing.T) {
+	if!wordpressContentWithValidBlogDomain.IsValid("", "", "") {
+		t.Error("Expected True")
+	}
+}
+
+func TestIsValidBlogDomain_False(t *testing.T) {
+	if wordpressContentWithInvalidBlogDomain.IsValid("", "", "") {
+		t.Error("Expected False")
+	}
+}
