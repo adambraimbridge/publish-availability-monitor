d98d0fc5f82c555c19819b2cd266e7c7477cee0a
Bogdan Guranda <bogdan.guranda@ft.com>
2017-04-28 10:31:56 +0300

    Fixed bug with logging external validation invalid when actually was valid.

--- content/content.go
+++ content/content.go
@@ -100,7 +100,7 @@ func doExternalValidation(p validationParam, validCheck func(int) bool, deletedC
 		log.Warnf("External validation for content uuid=[%s] transaction_id=[%s] validationURL=[%s], reading response body error: [%v]", p.uuid, p.txID, p.validationURL, err)
 	}
 
-	if resp.StatusCode != http.StatusOK || resp.StatusCode != http.StatusNotFound {
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNotFound {
 		log.Infof("External validation for content uuid=[%s] transaction_id=[%s] validationURL=[%s], received statusCode [%d], received error: [%v]", p.uuid, p.txID, p.validationURL, resp.StatusCode, string(bs))
 	}
 
