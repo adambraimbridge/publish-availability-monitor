f0a03bded17718b4478b838b26c223ca059b4761
bogdanguranda <bogdan.guranda@gmail.com>
2017-10-24 13:50:24 +0300

    Refactored startup.sh added replacement for complementary content variable.

--- startup.sh
+++ startup.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-sed -i 's "KAFKA_TOPIC" "$KAFKA_TOPIC" ' /config.json
-sed -i 's "KAFKA_PROXY_HOST" "$KAFKA_PROXY_HOST" ' /config.json
-sed -i 's "QUEUE_ADDR" "$QUEUE_ADDR" ' /config.json
-sed -i 's "CONTENT_URL" "$CONTENT_URL" ' /config.json
-sed -i 's "COMPLEMENTARY_CONTENT_URL" "$COMPLEMENTARY_CONTENT_URL" ' /config.json
-sed -i 's "LISTS_URL" "$LISTS_URL" ' /config.json
-sed -i 's "LISTS_NOTIFICATIONS_URL" "$LISTS_NOTIFICATIONS_URL" ' /config.json
-sed -i 's "NOTIFICATIONS_URL" "$NOTIFICATIONS_URL" ' /config.json
-sed -i 's "LISTS_NOTIFICATIONS_PUSH_URL" "$LISTS_NOTIFICATIONS_PUSH_URL" ' /config.json
-sed -i 's "NOTIFICATIONS_PUSH_URL" "$NOTIFICATIONS_PUSH_URL" ' /config.json
-sed -i 's "LISTS_NOTIFICATIONS_PUSH_API_KEY" "$LISTS_NOTIFICATIONS_PUSH_API_KEY" ' /config.json
-sed -i 's "NOTIFICATIONS_PUSH_API_KEY" "$NOTIFICATIONS_PUSH_API_KEY" ' /config.json
-sed -i 's "INTERNAL_COMPONENTS_URL" "$INTERNAL_COMPONENTS_URL" ' /config.json
-sed -i 's "METHODE_ARTICLE_VALIDATION_URL" "$METHODE_ARTICLE_VALIDATION_URL" ' /config.json
-sed -i 's "METHODE_CONTENT_PLACEHOLDER_MAPPER_URL" "$METHODE_CONTENT_PLACEHOLDER_MAPPER_URL" ' /config.json
-sed -i 's "METHODE_LIST_VALIDATION_URL" "$METHODE_LIST_VALIDATION_URL" ' /config.json
-sed -i 's "METHODE_IMAGE_MODEL_MAPPER_URL" "$METHODE_IMAGE_MODEL_MAPPER_URL" ' /config.json
-sed -i 's "METHODE_ARTICLE_INTERNAL_COMPONENTS_MAPPER_URL" "$METHODE_ARTICLE_INTERNAL_COMPONENTS_MAPPER_URL" ' /config.json
-sed -i 's "VIDEO_MAPPER_URL" "$VIDEO_MAPPER_URL" ' /config.json
-sed -i 's "WORDPRESS_MAPPER_URL" "$WORDPRESS_MAPPER_URL" ' /config.json
-sed -i 's "UUID_RESOLVER_URL" "$UUID_RESOLVER_URL" ' /config.json
+sed -i "s \"KAFKA_TOPIC\" \"$KAFKA_TOPIC\" " /config.json
+sed -i "s \"KAFKA_PROXY_HOST\" \"$KAFKA_PROXY_HOST\" " /config.json
+sed -i "s \"QUEUE_ADDR\" \"$QUEUE_ADDR\" " /config.json
+sed -i "s \"CONTENT_URL\" \"$CONTENT_URL\" " /config.json
+sed -i "s \"COMPLEMENTARY_CONTENT_URL\" \"$COMPLEMENTARY_CONTENT_URL\" " /config.json
+sed -i "s \"LISTS_URL\" \"$LISTS_URL\" " /config.json
+sed -i "s \"LISTS_NOTIFICATIONS_URL\" \"$LISTS_NOTIFICATIONS_URL\" " /config.json
+sed -i "s \"NOTIFICATIONS_URL\" \"$NOTIFICATIONS_URL\" " /config.json
+sed -i "s \"LISTS_NOTIFICATIONS_PUSH_URL\" \"$LISTS_NOTIFICATIONS_PUSH_URL\" " /config.json
+sed -i "s \"NOTIFICATIONS_PUSH_URL\" \"$NOTIFICATIONS_PUSH_URL\" " /config.json
+sed -i "s \"LISTS_NOTIFICATIONS_PUSH_API_KEY\" \"$LISTS_NOTIFICATIONS_PUSH_API_KEY\" " /config.json
+sed -i "s \"NOTIFICATIONS_PUSH_API_KEY\" \"$NOTIFICATIONS_PUSH_API_KEY\" " /config.json
+sed -i "s \"INTERNAL_COMPONENTS_URL\" \"$INTERNAL_COMPONENTS_URL\" " /config.json
+sed -i "s \"METHODE_ARTICLE_VALIDATION_URL\" \"$METHODE_ARTICLE_VALIDATION_URL\" " /config.json
+sed -i "s \"METHODE_CONTENT_PLACEHOLDER_MAPPER_URL\" \"$METHODE_CONTENT_PLACEHOLDER_MAPPER_URL\" " /config.json
+sed -i "s \"METHODE_LIST_VALIDATION_URL\" \"$METHODE_LIST_VALIDATION_URL\" " /config.json
+sed -i "s \"METHODE_IMAGE_MODEL_MAPPER_URL\" \"$METHODE_IMAGE_MODEL_MAPPER_URL\" " /config.json
+sed -i "s \"METHODE_ARTICLE_INTERNAL_COMPONENTS_MAPPER_URL\" \"$METHODE_ARTICLE_INTERNAL_COMPONENTS_MAPPER_URL\" " /config.json
+sed -i "s \"VIDEO_MAPPER_URL\" \"$VIDEO_MAPPER_URL\" " /config.json
+sed -i "s \"WORDPRESS_MAPPER_URL\" \"$WORDPRESS_MAPPER_URL\" " /config.json
+sed -i "s \"UUID_RESOLVER_URL\" \"$UUID_RESOLVER_URL\" " /config.json
 
 exec ./publish-availability-monitor -config /config.json -etcd-peers $ETCD_PEERS
