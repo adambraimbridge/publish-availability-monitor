bbb3fea4b7a7479059d60c8806913836c78bff80
Erdem Aslan <erdem.aslan@ft.com>
2018-08-08 17:26:25 +0100

    fmt'ed

--- publishCheck_content_neo4j_test.go
+++ publishCheck_content_neo4j_test.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIsCurrentOperationFinished_ContentNeo4jCheck_InvalidContent(t *testing.T) {
